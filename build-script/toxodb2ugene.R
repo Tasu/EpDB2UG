a7e10bcf5d52edc7d5ce839ddfec109b0e7c5958
tatsuki.sugi@gmail.com <tatsuki@SugiTatsuki-Air.home>
2016-08-01 09:39:57 -0400

    sample script for using this package added

--- build-script/toxodb2ugene.R
+++ build-script/toxodb2ugene.R
@@ -1,24 +1,24 @@
 #modified 052716TASUGI
-
-######please change these 4 parameter for your settings.###########
-#1.   input gff file name
-in_f<-"~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/TGME49_chrIb_823555_857088.gff3"
-
-#2.   fasta file name you will use for ugene
-outFASTAUGENE<-"~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/TGME49_chrIb_823555_857088.fasta"
-
-#3.   gff file output you will use for ugene
-outGFFUGENE<-"~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/TGME49_chrIb_823555_857088.ugene.gff3"
-
-###############change if needed. DEFAULT<-current directory+temp#####
-#5. tempfile prefix
-tempDir<-"./temp"
-#gff temp output file, you can ignore this
-outGFFtemp<-paste(tempDir,runif(1),sep="")
-#####################################################################
+#modified 073016Tasu
 
 #read requirement
 library(EpDB2UG)
+
+######please change these 4 parameter for your settings.###########
+#1.   input gff file name
+in_f<-"~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/temp/TGME49_chrVIIa_2403098_2413097.gff3"
+
+###############change if needed. DEFAULT<-current directory+temp#####
+#2.   fasta file name you will use for ugene
+outFASTAUGENE<-paste(in_f,".fasta",sep="")
+#3.   gff file output you will use for ugene
+outGFFUGENE<-paste(in_f,".ugene.gff",sep="")
+#4. tempfile prefix
+tempDir<-"./temp"
+outGFFtemp<-paste(tempDir,runif(1),sep="")
+#####################################################################
+
+
 
 #run parser
 parsegff3WithSeq(inF=in_f,outGFF=outGFFtemp,outFASTA=outFASTAUGENE)
@@ -29,3 +29,4 @@ file.remove(outGFFtemp)
 
 #after running this script without error message...
 #Please read GFF file for UGENE and fasta file in UGENE and HF.
+
