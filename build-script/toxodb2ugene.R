00d823f702a926f8272b343f93cc1321d59abee6
tatsuki.sugi@gmail.com <tatsuki@SugiTatsuki-Air.home>
2016-08-04 01:40:13 -0400

    devide script for single or batch process

--- build-script/toxodb2ugene.R
+++ build-script/toxodb2ugene.R
@@ -2,22 +2,30 @@
 #modified 073016Tasu
 #modified 080216Tasu batch conversion example added.
 
-#read requirement
+#install requirement
+install.packages("devtools")
+library(devtools)
+install_github("Tasu/EpDB2UG")
+
+#####################################################################
+####batch usage: if you have multiple gff3 files in one directory.###
+#####################################################################
 library(EpDB2UG)
-####batch process #not recommended###
+#change to your target directory
 dirL<-("~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/temp/")
 flist<-dir(dirL)
 files=paste(dirL,flist[grepl("*gff3$",flist)],sep="")
-files
 sapply(files,toxodb2ugene)
-###################################
+#############batch usage: END HERE###################################
 
-####standard example
-######please change this parameter for your settings.###########
+
+###################################################################
+####standard usage: if you want to convert one gff3 file###########
+###################################################################
+library(EpDB2UG)
 #1.   input gff file name
 in_f<-"~/Google Drive/louLab/cyst wall/figures-6umclearance/tagging/temp/TGME49_chrVIII_5677701_5687700.gff3"
-
-###############change if needed.#####
+#change if needed. DEFAULT: output files will be made in the same directry tha you put input file#####
 #2.   fasta file name you will use for ugene
 outFASTAUGENE<-paste(in_f,".fasta",sep="")
 #3.   gff file output you will use for ugene
@@ -25,14 +33,10 @@ outGFFUGENE<-paste(in_f,".ugene.gff",sep="")
 #4. tempfile prefix
 tempDir<-"./temp"
 outGFFtemp<-paste(tempDir,runif(1),sep="")
-#####################################################################
 #run parser
 parsegff3WithSeq(inF=in_f,outGFF=outGFFtemp,outFASTA=outFASTAUGENE)
 #edit base position
 modifyGFF4UGENE(toxoDBGFF=outGFFtemp, UGENEGFF=outGFFUGENE)
 #delete temp file
 file.remove(outGFFtemp)
-
-#after running this script without error message...
-#Please read GFF file for UGENE and fasta file in UGENE and HF.
-
+#################single usage: END HERE##########################################
