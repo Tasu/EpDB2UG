beb17343d4cab1442395f388ed4bde8374b0f672
Tasu <tatsuki@SugiTatsuki-MacBook-Air.local>
2016-08-12 12:03:22 -0400

    test for refactoring added

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -1,11 +1,16 @@
 #testthat test
-context(getwd())
+context(paste("test for refactoring, output file should be consistent, test working dir is", getwd()))
+cat("\nloading \"tools\" package for md5sum\n")
+library(tools)#for md5 check, only for testthat
 #####
 test_that("toxodb2ugene normal function for refactoring", {
-  checksumExpect<-md5sum("./testdata/expect/testTGME49_chrX_1032284_1067339.gff3.gb")
+  input<-"./testdata/input/"
+  checksumExpect<-md5sum(files = "./testdata/expect/testTGME49_chrX_1032284_1067339.gff3.gb")
   #test
   toxodb2ugene("./testdata/input/testTGME49_chrX_1032284_1067339.gff3")
   checksupTest<-md5sum("./testdata/input/testTGME49_chrX_1032284_1067339.gff3.gb")
-  file.remove("./testdata/input/testTGME49_chrX_1032284_1067339.gff3.gb")
-  expect_that(checksupTest, equals(checksumExpect))
+  fs<-dir(input)
+  fs<-paste(input,fs[!grepl("gff3$",fs)],sep="")
+  file.remove(fs)
+  expect_that(as.character(checksupTest[1]), equals(as.character(checksumExpect[1])))
 })
