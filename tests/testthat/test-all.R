fefa0ada79d3cb359128db0e7ab1fa800fe0f920
Tasu <tatsuki@SugiTatsuki-MacBook-Air.local>
2016-08-12 11:38:07 -0400

    test case during refactoring
    output file should be exactly same as before

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -1,16 +1,11 @@
 #testthat test
 context(getwd())
 #####
-#file output should be separated from package function.
-#package function will be modified to return file object only.
-#Or split function to make object and write file.
-#making object can be tested easily.
-test_that("toxodb2ugene normal function", {
-  #expect_that(toxodb2ugene("./testdata/input/testTGME49_chrX_1032284_1067339.gff3"), equals(1))
-})
-test_that("toxodb2ugene input file error", {
-})
-test_that("toxodb2ugene input file format error", {
-})
-test_that("toxodb2ugene output file error", {
+test_that("toxodb2ugene normal function for refactoring", {
+  checksumExpect<-md5sum("./testdata/expect/testTGME49_chrX_1032284_1067339.gff3.gb")
+  #test
+  toxodb2ugene("./testdata/input/testTGME49_chrX_1032284_1067339.gff3")
+  checksupTest<-md5sum("./testdata/input/testTGME49_chrX_1032284_1067339.gff3.gb")
+  file.remove("./testdata/input/testTGME49_chrX_1032284_1067339.gff3.gb")
+  expect_that(checksupTest, equals(checksumExpect))
 })
